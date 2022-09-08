a7b56fe1812740e51ccabe9cddc5a8e58a325514
Ojeyinka Abdul Azeez <ojeyinkaabdulazeez@Ojeyinkas-MacBook-Air.local>
2022-09-08 02:41:41 +0100

    Initial Commit of flutter notesapp to main branch

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:notesapp/firebase_options.dart';
+import 'package:notesapp/views/login_view.dart';
+// import 'package:notesapp/views/login_view.dart';
+// import 'package:notesapp/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              //   return const Text("Done");
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Please Verify your Email Address"),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text("Send Email Verification"),
+        ),
+      ],
+    );
+  }
+}
