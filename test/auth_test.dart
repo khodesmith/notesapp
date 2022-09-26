bb503767d34fce5ef2537151ac88168b4ee18b81
Ojeyinka Abdul Azeez <azeezojeyinka@yahoo.com>
2022-09-26 20:24:27 +0100

    main test code done

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -3,7 +3,85 @@ import 'package:notesapp/services/auth/auth_provider.dart';
 import 'package:notesapp/services/auth/auth_user.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("Mock Authentication", () {
+    final provider = MockAuthProvider();
+
+    test("Should not be initialized to begin with", () {
+      expect(
+        provider.isInitialized,
+        false,
+      );
+    });
+
+    test("Cannot logout if not initialized", () {
+      expect(
+        provider.logOut(),
+        throwsA(const TypeMatcher<NotInitializedException>()),
+      );
+    });
+
+    test(
+      "Should be able to initialized",
+      () async {
+        await provider.initialize();
+        expect(provider.isInitialized, true);
+      },
+    );
+
+    test("User should be null after initialization", () {
+      expect(provider.currentUser, null);
+    });
+
+    test(
+      "Should be able to initialize in less than 2 seconds",
+      () async {
+        await provider.initialize();
+        expect(provider.isInitialized, true);
+      },
+      timeout: const Timeout(Duration(seconds: 2)),
+    );
+
+    test("Create User should delegate to logIn function", () async {
+      final badEmailUser = provider.createUser(
+        email: "supersun@gmail.com",
+        password: "anypass",
+      );
+
+      expect(badEmailUser,
+          throwsA(const TypeMatcher<UserNotFoundAuthException>()));
+
+      final badPasswordUser =
+          provider.createUser(email: "anymail@gmail.com", password: "super");
+
+      expect(badPasswordUser,
+          throwsA(const TypeMatcher<WrongPasswordAuthException>()));
+
+      final user =
+          await provider.createUser(email: "email", password: "password");
+
+      expect(provider.currentUser, user);
+      expect(user.isEmailVerified, false);
+    });
+
+    test("Logged in user should be able to get verified", () {
+      provider.sendEmailVerification();
+      final user = provider.currentUser;
+      expect(user, isNotNull);
+      expect(user!.isEmailVerified, true);
+    });
+
+    test("Should be able to log in and log out again", () async {
+      await provider.logOut();
+      await provider.login(
+        email: "email",
+        password: "password",
+      );
+      final user = provider.currentUser;
+      expect(user, isNotNull);
+    });
+  });
+}
 
 class NotInitializedException implements Exception {}
 
