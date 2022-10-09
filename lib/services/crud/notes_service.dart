08688d1c533a5e2bd62798ef6f898ae023350bb0
Ojeyinka Abdul Azeez <azeezojeyinka@yahoo.com>
2022-10-09 15:30:35 +0100

    add create user and note table sql

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/foundation.dart';
 import "package:sqflite/sqflite.dart";
-import 'package:path_provider/path_provider.dart'
-    show getApplicationDocumentsDirectory;
+import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class NotesService {
+  Database? _db;
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      _db = db;
+
+//create the user table
+      const createuserTable = '''
+          CREATE TABLE IF NOT EXISTS "user" (
+            "id"	INTEGER NOT NULL,
+            "email"	TEXT NOT NULL UNIQUE,
+            PRIMARY KEY("id" AUTOINCREMENT)
+          );
+''';
+
+      await db.execute(createuserTable);
+
+//create note table
+      const createnoteTable = '''
+                CREATE TABLE IF NOT EXISTS "note" (
+                  "id"	INTEGER NOT NULL,
+                  "user_id"	INTEGER NOT NULL,
+                  "note_text"	TEXT,
+                  "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
+                  FOREIGN KEY("user_id") REFERENCES "user"("id"),
+                  PRIMARY KEY("id" AUTOINCREMENT)
+                );
+              ''';
+      await db.execute(createnoteTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentsDirectory();
+    }
+  }
+}
 
 @immutable
 class DatabaseUser {
@@ -58,6 +103,10 @@ class DatabaseNote {
   @override
   int get hashCode => id.hashCode;
 }
+
+const dbName = "notes.db";
+const notesTable = "note";
+const userTable = "user";
 
 const idColumn = "id";
 const emailColumn = "email";
